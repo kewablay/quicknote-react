7139d404c3b95d1faec857818d04c2b495e9865e
kewablay <kojokewajunior123@gmail.com>
2021-12-22 19:13:34 +0000

    added animations

--- src/components/GetStartedModal.js
+++ src/components/GetStartedModal.js
@@ -1,16 +1,48 @@
 import React from "react";
 import getStartedGif from "../images/getstarted.gif";
+import { motion } from "framer-motion";
 
 export const GetStartedModal = ({ started, setStarted }) => {
+  const backdropVariant = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 0.2,
+      },
+    },
+  };
+
+  const modalVariant = {
+    hidden: {
+      opacity: 0,
+      y: -200,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: .5,
+      },
+    },
+  };
+
   return (
-    <div className="backdrop">
-      <div className="get-started">
+    <motion.div
+      className="backdrop"
+      variants={backdropVariant}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.div className="get-started" variants={modalVariant}>
         <p>Add new note</p>
         <img src={getStartedGif} alt="getStarted" id="get-started-img" />
         <button className="got-it" onClick={() => setStarted(false)}>
           Got it
         </button>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
